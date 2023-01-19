7bde0ddda0047ff6702f94b8d1f4d2f3165c677e
xmozaffary <121026815+xmozaffary@users.noreply.github.com>
2023-01-19 09:01:23 +0100

    Update lock.c

--- lock_ali/lock.c
+++ lock_ali/lock.c
@@ -5,15 +5,15 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo;                  // skappa servo object för att Kontrollera servon
 
 int pos = 0;
 
-#define Password_length 5 //längd av lössenordet + null karaktär
-char Data[Password_length]; // Karakär för att hålla lössenordet 
-char Master[Password_length] = "1234"; // själva Lössenordet 
-byte date_count = 0; // räknare för Karaktär som är 0 från början
-char key; // den håller karaktär som matas in från keypad
+#define Password_length 5         //längd av lössenordet + null karaktär
+char Data[Password_length];       // Karakär för att hålla lössenordet 
+char Master[Password_length] = "1234";      // själva Lössenordet 
+byte date_count = 0;                   // räknare för Karaktär som är 0 från början
+char key;           // den håller karaktär som matas in från keypad
 
 const uint8_t ROWS = 4;
 const uint8_t COLS = 4;
@@ -24,8 +24,8 @@ char keys[ROWS][COLS] = {
   { '*', '0', '#', 'D' }
 };
 
-uint8_t colPins[COLS] = { 5, 4, 3, 2 }; // Pins connected to C1, C2, C3, C4
-uint8_t rowPins[ROWS] = { 9, 8, 7, 6 }; // Pins connected to R1, R2, R3, R4
+uint8_t colPins[COLS] = { 5, 4, 3, 2 }; // Pinar kopplad till 5, 4, 3, 2
+uint8_t rowPins[ROWS] = { 9, 8, 7, 6 }; // Pinar kopplad till 9, 8, 7, 6
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
@@ -40,13 +40,13 @@ void setup() {
 void loop() {
   lcd.setCursor(0, 0);
   lcd.print(" Enter PassWord");
-  static char keyIn[4]; // Save the input characters in array size 4
-  static byte keyInNum = 0; // How many key press?
+  static char keyIn[4];       // spara inputen characters i array size 4
+  static byte keyInNum = 0;   // räknarn hur många knapp tryckas
   char key = keypad.getKey();
 
   if(key)
   {
-    Data[date_count] = key;
+    Data[date_count] = key; 
     lcd.setCursor(date_count, 1);
     lcd.print(Data[date_count]);
     Serial.println(Data[date_count]);
@@ -56,7 +56,7 @@ void loop() {
   {
     if (!strcmp(Data, Master)) // här jämförs (Data som matas in  ) och (Master som är riktiga lössenordet)
     {
-      myservo.write(pos);             // tell servo to go to position in variable 'pos'
+      myservo.write(pos);             // servo ska i position 'pos' (int variable)
       lcd.print("  Corect");
       delay(2000);
       myservo.write(90); 
